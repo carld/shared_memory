51271606fc354afd320a82893b8f82b6ada7395f
Carl <carl.douglas@gmail.com>
2016-07-24 20:30:28 +1200

    Get pixels from shared memory

--- shm_window/main.c
+++ shm_window/main.c
@@ -3,21 +3,56 @@
 
 #include <SDL2/SDL.h>
 
+
+#include <sys/mman.h>
+#include <fcntl.h>
+#include <unistd.h>
+
 unsigned width = 640;
 unsigned height = 480;
-unsigned int pause = 0;
 unsigned int frames = 0;
 unsigned int start_tick;
+
+#define NAME "shm_pixels"
 
 int main(int argc, char *argv[]) {
   SDL_Event ev;
   SDL_Window *window = 0;
+  SDL_Renderer *renderer = 0;
+  SDL_Texture *texture = 0;
+
+  int fd = 0;
+  void *pixels = 0;
+
+  if (shm_unlink(NAME) < 0) {
+    perror("shm_unlink");
+  }
+  fd = shm_open(NAME, O_CREAT | O_RDWR, S_IRUSR | S_IWUSR);
+  if (fd < 0) {
+    perror("shm_open");
+    exit(-1);
+  }
+  if (ftruncate(fd, width * height * 4) < 0) {
+    perror("ftruncate");
+  }
+
+  pixels = mmap(0, width * height * 4, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+  if (pixels == 0) {
+    perror("mmap");
+    exit(-1);
+  }
+
 
   if (SDL_Init(SDL_INIT_VIDEO|SDL_INIT_TIMER) != 0) {
     SDL_Log("Failed to initialize SDL: %s\n", SDL_GetError());
   }
 
-  window = SDL_CreateWindow("shm_window", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, width, height, 0);
+  window = SDL_CreateWindow(NAME, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, width, height, 0);
+
+  renderer = SDL_CreateRenderer(window, -1, 0);
+
+  texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888,
+      SDL_TEXTUREACCESS_TARGET, width, height);
 
   SDL_ShowCursor(1);
 
@@ -26,16 +61,25 @@ int main(int argc, char *argv[]) {
     if (ev.type == SDL_KEYDOWN) {
       if (ev.key.keysym.sym == SDLK_ESCAPE)
         break;
-      if (ev.key.keysym.sym == SDLK_SPACE)
-        pause = !pause;
     }
     if (ev.type == SDL_QUIT)
       break;
     SDL_Delay(20);
+
+    SDL_UpdateTexture(texture, NULL, pixels, width * sizeof (Uint32));
+    SDL_RenderClear(renderer);
+    SDL_RenderCopy(renderer, texture, NULL, NULL);
+    SDL_RenderPresent(renderer);
   }
 
+  SDL_DestroyTexture(texture);
+  SDL_DestroyRenderer(renderer);
   SDL_DestroyWindow(window);
   SDL_Quit();
+
+  shm_unlink(NAME);
+  munmap(pixels, width * height * 4);
+  close(fd);
 
   return 0;
 }
